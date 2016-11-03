6bab47dbf3c38816b06d3aec491dfb898c26aeb3
Mr-Un1k0d3r <mr.un1k0d3r@gmail.com>
2016-11-03 11:55:29 -0400

    Create Remote-WmiExecute.ps1

---
+++ Remote-WmiExecute.ps1
@@ -0,0 +1,27 @@
+function Remote-WmiExecute {
+	# Charles F. Hamilton - Mandiant 2016
+	# Execute command through WMI on a remote computer
+	
+	param(
+	[Parameter(Mandatory=$True)]
+	[string]$Payload,
+	[Parameter(Mandatory=$True)]
+	[string]$ComputerName
+	)
+	
+	BEGIN {
+		Write-Host "[+] Executing payload on $($ComputerName)"
+	}
+	
+	PROCESS {
+		$process = Invoke-WmiMethod -ComputerName $ComputerName -Class Win32_Process -Name Create -ArgumentList $Payload
+		Register-WmiEvent -ComputerName $ComputerName -Query "Select * from Win32_ProcessStopTrace Where ProcessID=$($process.ProcessId)" -Action {
+			$state = $event.SourceEventArgs.NewEvent;
+			Write-Host "`n[+] Remote process status:`nPID: $($state.ProcessId)`nState: $($state.State)`nStatus: $($state.Status)" 
+		}
+	}
+	
+	END {
+		Write-Host "[+] Process completed..."
+	}
+}
