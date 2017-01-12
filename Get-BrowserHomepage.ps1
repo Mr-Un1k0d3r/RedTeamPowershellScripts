48366b2070bea8fe7a124d3c9c4febfb591fbdf5
Mr-Un1k0d3r <mr.un1k0d3r@gmail.com>
2017-01-12 12:18:36 -0500

    Update Get-BrowserHomepage.ps1

--- Get-BrowserHomepage.ps1
+++ Get-BrowserHomepage.ps1
@@ -1,5 +1,5 @@
 function Get-BrowserHomepage {
-	# Charles F. Hamilton - Mandiant 2016
+	# Mr.Un1k0d3r - RingZer0 Team 2016
 	# Get Browser Homepage
 
   BEGIN {
@@ -7,7 +7,7 @@ function Get-BrowserHomepage {
 	}
 	
 	PROCESS {
-    Get-ItemProperty -Path "HKCU:\Software\Microsoft\Internet Explorer\Main\" -Name "start page" | Select "Start Page"
+    		Get-ItemProperty -Path "HKCU:\Software\Microsoft\Internet Explorer\Main\" -Name "start page" | Select "Start Page"
 	}
 	
 	END {
