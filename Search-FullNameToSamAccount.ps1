97e5cbd25c5b63f1d8c9aafc18fb84c89b6b7945
Mr-Un1k0d3r <mr.un1k0d3r@gmail.com>
2017-04-27 14:54:21 -0400

    Create Search-FullNameToSamAccount.ps1

---
+++ Search-FullNameToSamAccount.ps1
@@ -0,0 +1,26 @@
+Import-Module ActiveDirectory
+
+function Search-FullNameToSamAccount {
+	# Mr.Un1k0d3r - RingZer0 Team 2016
+	# Get SamAccountName using Full name search
+  # New ActiveDirectory module
+	param(
+	  [Parameter(Mandatory=$True, ValueFromPipeline=$true)]
+    [string]$Filter
+  )
+  
+  BEGIN {
+    Write-Host "Searching for $($Filter)"
+	}
+	
+	PROCESS {
+        $Users = Get-ADUser -Filter{displayName -like $Filter -and Enabled -eq $True} -Properties SamAccountName
+    		ForEach($user in $Users) {
+          Write-Host $user.SamAccountName
+        }
+	}
+	
+	END {
+		Write-Host "[+] Process completed..."
+	}
+}
