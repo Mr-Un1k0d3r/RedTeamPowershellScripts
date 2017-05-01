d5ee11d5a44c1d86c5862640b3444404fc99dc5c
Mr-Un1k0d3r <mr.un1k0d3r@gmail.com>
2017-05-01 11:41:05 -0400

    Update Search-FullNameToSamAccount.ps1

--- Search-FullNameToSamAccount.ps1
+++ Search-FullNameToSamAccount.ps1
@@ -1,5 +1,5 @@
 function Search-FullNameToSamAccount {
-	# Mr.Un1k0d3r - RingZer0 Team 2016
+	# Mr.Un1k0d3r - RingZer0 Team 2017
 	# Get SamAccountName using Full name search
 	# dependencies ActiveDirectory module
 	
@@ -9,7 +9,7 @@ function Search-FullNameToSamAccount {
 	)
   
 	BEGIN {
-		$module = Get-Module -List ActiveDirectoryA
+		$module = Get-Module -List ActiveDirectory
 		if($module) {
 			Import-Module ActiveDirectory
 		} else {
@@ -19,7 +19,7 @@ function Search-FullNameToSamAccount {
 	}
 	
 	PROCESS {
-        $Users = Get-ADUser -Filter{displayName -like $Filter -and Enabled -eq $True} -Properties SamAccountName, displayName
+        	$Users = Get-ADUser -Filter{displayName -like $Filter -and Enabled -eq $True} -Properties SamAccountName, displayName
 		ForEach($user in $Users) {
 			Write-Host "[+] Found: $($user.displayName) -> $($user.SamAccountName)"
 		}
