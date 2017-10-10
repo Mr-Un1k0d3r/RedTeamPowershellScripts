af0b0a50060b7261994b657c4da754a32ef8b25f
Mr.Un1k0d3r <mr.un1k0d3r@gmail.com>
2017-10-10 11:05:20 -0400

    Update Search-Users.ps1

--- scripts/Search-Users.ps1
+++ scripts/Search-Users.ps1
@@ -51,38 +51,44 @@ function Search-EventForUser {
 	}
 }
 
-
 function Search-FullNameToSamAccount {
 	# Mr.Un1k0d3r - RingZer0 Team 2017
-	# Get SamAccountName using Full name search
-	# dependencies ActiveDirectory module
-	
+	# Get SamAccountName using displayname property search
 	param(
 		[Parameter(Mandatory=$True, ValueFromPipeline=$true)]
 		[string]$Filter
 	)
-  
+
 	BEGIN {
-		$module = Get-Module -List ActiveDirectory
-		if($module) {
-			Import-Module ActiveDirectory
-		} else {
-			throw "[-] ERROR: ActiveDirectory cannot be imported. Aborting..."
-		}
+		$Users = @()
 		Write-Output "[*] Searching for $($Filter)"
 	}
-	
+
 	PROCESS {
-        	$Users = Get-ADUser -Filter{displayName -like $Filter -and Enabled -eq $True} -Properties SamAccountName, displayName
-		if($Users) {
-			ForEach($user in $Users) {
-				Write-Output "[+] Found: $($user.displayName) -> $($user.SamAccountName)"
+		$Query = "(&(objectCategory=User)(displayName=*$($Filter)*))"
+		$Domain = New-Object System.DirectoryServices.DirectoryEntry
+
+		$DirSearch = New-Object System.DirectoryServices.DirectorySearcher
+		$DirSearch.SearchRoot = $Domain
+		$DirSearch.PageSize = 100
+		$DirSearch.Filter = $Query
+		$DirSearch.SearchScope = "Subtree"
+
+		ForEach($Item in $DirSearch.FindAll()) {
+			$User = $Item.Properties
+			$Output = New-Object -TypeName PSObject -Property @{
+				Name = $User.givenname[0]
+				SamAccount = $User.samaccountname[0]
+				Department = $User.department[0]
+				Description = $User.description[0]
 			}
-		} else {
-			Write-Output "[-] Search filter returned nothing..."
+
+			$Users += $Output
 		}
+
+		$Users | Format-Table -Wrap -AutoSize
 	}
-	
+
 	END {
 		Write-Output "[*] Process completed..."
 	}
