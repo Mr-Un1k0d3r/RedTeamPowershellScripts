6df6212f0d537232b011dc08a6be8a9db07e6bf3
Mr.Un1k0d3r <mr.un1k0d3r@gmail.com>
2017-10-11 11:30:41 -0400

    Update Search-Users.ps1

--- scripts/Search-Users.ps1
+++ scripts/Search-Users.ps1
@@ -97,3 +97,64 @@ function Search-FullNameToSamAccount {
 		Write-Output "[*] Process completed..."
 	}
 }
+
+function Ldap-GetProperty {
+	param(
+	[Parameter(Mandatory=$True, ValueFromPipeline=$True)]
+	[string]$Filter,
+	[Parameter(Mandatory=$True)]
+	[string]$Property
+	)
+	
+	BEGIN {
+		$Output = @()
+	}
+	
+	PROCESS {
+		$Domain = New-Object System.DirectoryServices.DirectoryEntry
+
+		$DirSearch = New-Object System.DirectoryServices.DirectorySearcher
+		$DirSearch.SearchRoot = $Domain
+		$DirSearch.PageSize = 100
+		$DirSearch.Filter = $Filter
+		$DirSearch.SearchScope = "Subtree"	
+		
+		ForEach($Item in $DirSearch.FindAll()) {
+			$Data = $Item.Properties
+			Try {
+			$Element = New-Object -TypeName PSObject -Property @{
+				$Property = $Data.$Property[0]
+			}
+
+			$Output += $Element		
+			} Catch {
+				Write-Output "[-] Property not found"
+			}
+		}
+		return $Output
+	}
+	
+}
+
+function Search-UserPassword {
+	
+	param(
+	[Parameter(Mandatory=$True, ValueFromPipeline=$True)]
+	[string]$UserName
+	)
+	
+	BEGIN {
+	
+	}
+	
+	PROCESS {
+		ForEach($User in $UserName) {
+			Write-Output "[*] $($User)"
+			Ldap-GetProperty -Filter "(&(objectCategory=User)(samaccountname=*$($User)*))" -Property "userpassword" | Format-Table -Wrap -AutoSize
+		}
+	}
+	
+	END {
+		Write-Output "[+] Process completed..."
+	}
+}
