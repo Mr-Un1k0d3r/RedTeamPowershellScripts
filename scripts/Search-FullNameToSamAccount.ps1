7a0533d4141344707212bfa994a3c5ab9b5f252f
Mr.Un1k0d3r <mr.un1k0d3r@gmail.com>
2017-10-11 11:22:10 -0400

    Update Search-FullNameToSamAccount.ps1

--- scripts/Search-FullNameToSamAccount.ps1
+++ scripts/Search-FullNameToSamAccount.ps1
@@ -1,39 +1,43 @@
 function Search-FullNameToSamAccount {
 	# Mr.Un1k0d3r - RingZer0 Team 2017
 	# Get SamAccountName using displayname property search
+	
 	param(
-		[Parameter(Mandatory=$True, ValueFromPipeline=$true)]
+		[Parameter(Mandatory=$True, ValueFromPipeline=$True)]
 		[string]$Filter
 	)
 
 	BEGIN {
 		$Users = @()
-		Write-Output "[*] Searching for $($Filter)"
 	}
 
 	PROCESS {
-		$Query = "(&(objectCategory=User)(displayName=*$($Filter)*))"
-		$Domain = New-Object System.DirectoryServices.DirectoryEntry
+		ForEach($User in $Filter) {
+			Write-Output "[*] Searching for $($User)"
+			$Query = "(&(objectCategory=User)(displayName=*$($User)*))"
+			$Domain = New-Object System.DirectoryServices.DirectoryEntry
 
-		$DirSearch = New-Object System.DirectoryServices.DirectorySearcher
-		$DirSearch.SearchRoot = $Domain
-		$DirSearch.PageSize = 100
-		$DirSearch.Filter = $Query
-		$DirSearch.SearchScope = "Subtree"
+			$DirSearch = New-Object System.DirectoryServices.DirectorySearcher
+			$DirSearch.SearchRoot = $Domain
+			$DirSearch.PageSize = 100
+			$DirSearch.Filter = $Query
+			$DirSearch.SearchScope = "Subtree"
 
-		ForEach($Item in $DirSearch.FindAll()) {
-			$User = $Item.Properties
-			$Output = New-Object -TypeName PSObject -Property @{
-				Name = $User.givenname[0]
-				SamAccount = $User.samaccountname[0]
-				Department = $User.department[0]
-				Description = $User.description[0]
+			ForEach($Item in $DirSearch.FindAll()) {
+				$Data = $Item.Properties
+				$Output = New-Object -TypeName PSObject -Property @{
+					Name = $Data.givenname[0]
+					SamAccount = $Data.samaccountname[0]
+					Department = $Data.department[0]
+					Description = $Data.description[0]
+				}
+
+				$Users += $Output
 			}
-
-			$Users += $Output
+			$Users | Format-Table -Wrap -AutoSize
+			$Users = @()
 		}
-
-		$Users | Format-Table -Wrap -AutoSize
+		
 	}
 
 	END {
