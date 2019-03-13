67024f886d0ee427e4cc3842d09b6f316f22d248
Mr.Un1k0d3r <mr.un1k0d3r@gmail.com>
2019-03-13 12:56:48 -0400

    Update COM-Utility.ps1

--- scripts/COM-Utility.ps1
+++ scripts/COM-Utility.ps1
@@ -2,8 +2,6 @@ function Invoke-COM-ShellApplication {
 
 	param(
 		[Parameter(Mandatory=$True)]
-		[string]$ComputerName,
-		[Parameter(Mandatory=$True)]
 		[string]$Command,
 		[Parameter(Mandatory=$True)]
 		[string]$Argument
@@ -11,11 +9,11 @@ function Invoke-COM-ShellApplication {
 
 	BEGIN {
 		$GUID = "13709620-C279-11CE-A49E-444553540000"
-		Write-Output "[+] Executing ShellApplication COM object on $($ComputerName)"
+		Write-Output "[+] Executing ShellApplication COM object"
 	}
 	
 	PROCESS {
-		$Instance = [activator]::CreateInstance([type]::GetTypeFromCLSID($GUID, $ComputerName))
+		$Instance = [activator]::CreateInstance([type]::GetTypeFromCLSID($GUID))
 		$Instance.ShellExecute($Command, $Argument, "", "", 0)
 	}
 	
@@ -28,8 +26,6 @@ function Invoke-COM-ProcessChain {
 
 	param(
 		[Parameter(Mandatory=$True)]
-		[string]$ComputerName,
-		[Parameter(Mandatory=$True)]
 		[string]$Command,
 		[Parameter(Mandatory=$True)]
 		[string]$Argument
@@ -37,11 +33,11 @@ function Invoke-COM-ProcessChain {
 
 	BEGIN {
 		$GUID = "E430E93D-09A9-4DC5-80E3-CBB2FB9AF28E"
-		Write-Output "[+] Executing ProcessChain COM object on $($ComputerName)"
+		Write-Output "[+] Executing ProcessChain COM object"
 	}
 	
 	PROCESS {
-		$Instance = [activator]::CreateInstance([type]::GetTypeFromCLSID($GUID, $ComputerName))
+		$Instance = [activator]::CreateInstance([type]::GetTypeFromCLSID($GUID))
 		$Instance.ExecutablePath = $Command
 		$Instance.CommandLine = $Argument
 		$Instance.Start([ref]$True)
@@ -57,8 +53,6 @@ function Invoke-COM-WindowsScriptHost {
 
 	param(
 		[Parameter(Mandatory=$True)]
-		[string]$ComputerName,
-		[Parameter(Mandatory=$True)]
 		[string]$Command,
 		[Parameter(Mandatory=$True)]
 		[string]$Argument
@@ -66,11 +60,11 @@ function Invoke-COM-WindowsScriptHost {
 
 	BEGIN {
 		$GUID = "F935DC22-1CF0-11D0-ADB9-00C04FD58A0B"
-		Write-Output "[+] Executing WindowsScriptHost COM object on $($ComputerName)"
+		Write-Output "[+] Executing WindowsScriptHost COM object"
 	}
 	
 	PROCESS {
-		$Instance = [activator]::CreateInstance([type]::GetTypeFromCLSID($GUID, $ComputerName))
+		$Instance = [activator]::CreateInstance([type]::GetTypeFromCLSID($GUID))
 		$Instance.Run($Command + " " + $Argument)
 	}
 	
@@ -83,18 +77,16 @@ function Invoke-COM-ShellBrowserWindow {
 
 	param(
 		[Parameter(Mandatory=$True)]
-		[string]$ComputerName,
-		[Parameter(Mandatory=$True)]
 		[string]$Command
 	)
 
 	BEGIN {
 		$GUID = "C08AFD90-F2A1-11D1-8455-00A0C91F3880"
-		Write-Output "[+] Executing ShellBrowserWindow COM object on $($ComputerName)"
+		Write-Output "[+] Executing ShellBrowserWindow COM object"
 	}
 	
 	PROCESS {
-		$Instance = [activator]::CreateInstance([type]::GetTypeFromCLSID($GUID, $ComputerName))
+		$Instance = [activator]::CreateInstance([type]::GetTypeFromCLSID($GUID))
 		$Instance.Navigate($Command)
 	}
 	
@@ -107,8 +99,6 @@ function Invoke-COM-XMLHTTP {
 
 	param(
 		[Parameter(Mandatory=$True)]
-		[string]$ComputerName,
-		[Parameter(Mandatory=$True)]
 		[string]$Path,
 		[Parameter(Mandatory=$False)]
 		[switch]$Execute = $False
@@ -116,11 +106,11 @@ function Invoke-COM-XMLHTTP {
 
 	BEGIN {
 		$GUID = "F5078F35-C551-11D3-89B9-0000F81FE221"
-		Write-Output "[+] Executing XML HTTP COM object on $($ComputerName)"
+		Write-Output "[+] Executing XML HTTP COM object"
 	}
 	
 	PROCESS {
-		$Instance = [activator]::CreateInstance([type]::GetTypeFromCLSID($GUID, $ComputerName))
+		$Instance = [activator]::CreateInstance([type]::GetTypeFromCLSID($GUID))
 		$Instance.Open("GET", $Path, $False)
 		$Instance.Send()
 		$Data = $Instance.responseText
@@ -140,8 +130,6 @@ function Invoke-COM-ScheduleService {
 
 	param(
 		[Parameter(Mandatory=$True)]
-		[string]$ComputerName,
-		[Parameter(Mandatory=$True)]
 		[string]$Command,
 		[Parameter(Mandatory=$True)]
 		[string]$Argument,
@@ -152,7 +140,7 @@ function Invoke-COM-ScheduleService {
 	BEGIN {
 		$ProgID = "Schedule.Service"
 		$TaskName = [Guid]::NewGuid().ToString()
-		Write-Output "[+] Executing ScheduleService COM object on $($ComputerName)"
+		Write-Output "[+] Executing ScheduleService COM object"
 		Write-Output "[+] Task name $($TaskName)"
 	}
 	
@@ -164,7 +152,7 @@ function Invoke-COM-ScheduleService {
 		$Task = $Instance.NewTask(0)
 		$Trigger = $Task.triggers.Create(0)
 		$Trigger.StartBoundary = Convert-Date -Date ((Get-Date).addSeconds($Delay))
-		$Trigger.EndBoundary = Convert-Date -Date ((Get-Date).addSeconds($Delay + 120))
+		$Trigger.EndBoundary = Convert-Date -Date ((Get-Date).addSeconds($Delay + (60 * 60)))
 		$Trigger.ExecutionTimelimit = "PT5M"
 		$Trigger.Enabled = $True
 		$Trigger.Id = $Taskname
