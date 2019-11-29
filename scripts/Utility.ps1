bdbc2b7010f43304d2c5d2134ed7af3f640b2724
Mr.Un1k0d3r <mr.un1k0d3r@gmail.com>
2019-11-29 17:59:04 -0500

    Update Utility.ps1

--- scripts/Utility.ps1
+++ scripts/Utility.ps1
@@ -314,3 +314,13 @@ function Dump-UserName {
 		Write-Output "[+] Process completed..."
 	}
 }
+
+function Resolve-DCtoIP {
+
+	PROCESS {
+		Write-Output "[+] Enumerating all the DCs"
+		ForEach($dc in [DirectoryServices.ActiveDirectory.Domain]::GetCurrentDomain().DomainControllers) {
+			Write-Output "[+] DC found: $($dc.Name):$($dc.IPAddress)"
+		}
+	}
+}
